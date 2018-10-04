0cf71fdc68e8bc0d1b0bc15ce5f8ec133bc404f2
Julien Fauville <julienfauville@Juliens-MacBook-Pro.local>
2018-10-04 16:57:44 +0200

    redis connect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate redis;
+// extern crate tokio_threadpool;
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
+use redis::{Client};
+// use tokio_threadpool::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+    info!("Service started");
+
+    let conn = Client::open("redis://localhos")
+        .map(|c| RedisConn { client: c })
+        .and_then(|rc| rc.client.get_connection());
+
+    match conn {
+        Ok(_) => info!("OK"),
+        _ => error!("Cannot connect to redis")
+    };
+}
+
+struct RedisConn {
+    client: Client
 }
